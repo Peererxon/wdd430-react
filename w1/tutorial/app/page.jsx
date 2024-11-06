19a1121f8a4b7a06167219e0827a83c7780d4747
andersongil <andersongil.dev@gmail.com>
2024-11-06 12:48:08 -0400

    Finishing basic react tutorial. From react to next

--- w1/tutorial/app/page.jsx
+++ w1/tutorial/app/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import LikeButton from "./like-button";
 
 function Header({ title }) {
 	return;
@@ -8,20 +8,16 @@ function Header({ title }) {
 
 export default function HomePage() {
 	const names = ["Ada Lovelace", "Grace Hopper", "Margaret Hamilton"];
-	const [likes, setLikes] = useState(0);
-	function handleClick() {
-		setLikes(likes + 1);
-	}
 	return (
 		<div>
 			<header title="Develop. Preview. Ship." />
 			<ul>
 				{names.map((name) => (
-					<li key="{name}">{name}</li>
+					<li key={name}>{name}</li>
 				))}
 			</ul>
 
-			<button onClick="{handleClick}">Like ({likes})</button>
+			<LikeButton />
 		</div>
 	);
 }
